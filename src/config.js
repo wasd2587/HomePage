eb0756b69b9ea9d97affb51d823dffd7163a06ad
zhang.jinshan <neusoft@600718>
2022-04-06 16:25:20 +0800

    first commit

---
+++ src/config.js
@@ -0,0 +1,64 @@
+const SERVE = false; // 是否使用线上服务器
+
+const HOST = ""; // 线上服务器地址，如果不使用请留空
+
+const BLOG_NAME = "EsunR-Blog"; // 个人网站名字
+
+const BLOG_URL = "https://blog.esunr.xyz"; // 个人网站链接（请填写完整链接）
+
+const GITHUB = "https://github.com/EsunR"; // 右上角 Github 图标链接，留空不显示
+
+const BACKGROUND_IMG_URL = ""; // 背景图片url，留空默认为必应每日图片
+
+const SORT_DATA = [
+  { sortId: 1, title: "工作" },
+  { sortId: 2, title: "生活" }
+]; // 网站分类列表，请按照API.md中'/getSort'接口中返回的data格式填写
+
+const RECORD_NUMBER = ""; // 网站备案号，留空不显示
+
+const PAGES_DATA = [
+  {
+    pageId: 1,
+    sortId: 1,
+    title: "工作",
+    subtitle: "美食食谱",
+    url: "http://www.wandermoon59.com:8999/",
+    icon: ""
+  },
+  {
+    pageId: 2,
+    sortId: 1,
+    title: "学习",
+    subtitle: "交响乐欣赏",
+    url: "http://www.wandermoon59.com:10086/",
+    icon: ""
+  }
+]; // 网站分类列表，请按照API.md中'/getPages'接口中返回的data格式填写
+
+const SLOGAN = [
+  "欢迎拜访",
+  "歡迎拜訪",
+  "訪問へようこそ",
+  "嗨，别来无恙",
+  "不忘初心，一生浪漫",
+  "最近还好吗？",
+  "流星，落花，萤火",
+  "马车越空，晃荡越响"
+]; // 在Logo下方显示的标题
+
+const FOOTER_INFO = true; // 是否显示抽屉右下角的文字信息
+
+export default {
+  SERVE,
+  HOST,
+  BLOG_URL,
+  BLOG_NAME,
+  SORT_DATA,
+  PAGES_DATA,
+  SLOGAN,
+  GITHUB,
+  FOOTER_INFO,
+  BACKGROUND_IMG_URL,
+  RECORD_NUMBER
+};
